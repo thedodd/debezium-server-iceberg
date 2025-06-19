3cbde92f401967db59c58935250c64d94ee9d76d
Ildar Musin <zildermann@gmail.com>
2025-06-19 18:31:16 +0200

    Table destination mapper

--- debezium-server-iceberg-sink/src/main/java/io/debezium/server/iceberg/IcebergChangeConsumer.java
+++ debezium-server-iceberg-sink/src/main/java/io/debezium/server/iceberg/IcebergChangeConsumer.java
@@ -15,6 +15,7 @@ import io.debezium.server.iceberg.batchsizewait.BatchSizeWait;
 import io.debezium.server.iceberg.converter.EventConverter;
 import io.debezium.server.iceberg.converter.JsonEventConverter;
 import io.debezium.server.iceberg.converter.StructEventConverter;
+import io.debezium.server.iceberg.mapper.IcebergTableMapper;
 import io.debezium.server.iceberg.tableoperator.IcebergTableOperator;
 import io.debezium.util.Clock;
 import io.debezium.util.Strings;
@@ -30,7 +31,6 @@ import org.apache.iceberg.CatalogUtil;
 import org.apache.iceberg.Schema;
 import org.apache.iceberg.Table;
 import org.apache.iceberg.catalog.Catalog;
-import org.apache.iceberg.catalog.Namespace;
 import org.apache.iceberg.catalog.TableIdentifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -69,6 +69,11 @@ public class IcebergChangeConsumer implements DebeziumEngine.ChangeConsumer<Embe
   @Inject
   GlobalConfig config;
 
+  @Inject
+  @Any
+  Instance<IcebergTableMapper> mappers;
+  IcebergTableMapper mapper;
+
   @PostConstruct
   void connect() {
     JsonEventConverter.initializeJsonSerde();
@@ -79,6 +84,7 @@ public class IcebergChangeConsumer implements DebeziumEngine.ChangeConsumer<Embe
     icebergCatalog = CatalogUtil.buildIcebergCatalog(config.iceberg().catalogName(), config.iceberg().icebergConfigs(), hadoopConf);
     batchSizeWait = IcebergUtil.selectInstance(batchSizeWaitInstances, config.batch().batchSizeWaitName());
     batchSizeWait.initizalize();
+    mapper = IcebergUtil.selectInstance(mappers, config.iceberg().mapper());
   }
 
   @Override
@@ -162,16 +168,6 @@ public class IcebergChangeConsumer implements DebeziumEngine.ChangeConsumer<Embe
   }
 
   public TableIdentifier mapDestination(String destination) {
-    final String tableName = destination
-        .replaceAll(config.iceberg().destinationRegexp().orElse(""), config.iceberg().destinationRegexpReplace().orElse(""))
-        .replace(".", "_");
-
-    if (config.iceberg().destinationUppercaseTableNames()) {
-      return TableIdentifier.of(Namespace.of(config.iceberg().namespace()), (config.iceberg().tablePrefix().orElse("") + tableName).toUpperCase());
-    } else if (config.iceberg().destinationLowercaseTableNames()) {
-      return TableIdentifier.of(Namespace.of(config.iceberg().namespace()), (config.iceberg().tablePrefix().orElse("") + tableName).toLowerCase());
-    } else {
-      return TableIdentifier.of(Namespace.of(config.iceberg().namespace()), config.iceberg().tablePrefix().orElse("") + tableName);
-    }
+      return mapper.mapDestination(destination);
   }
 }
--- debezium-server-iceberg-sink/src/main/java/io/debezium/server/iceberg/IcebergConfig.java
+++ debezium-server-iceberg-sink/src/main/java/io/debezium/server/iceberg/IcebergConfig.java
@@ -40,6 +40,10 @@ public interface IcebergConfig {
   @WithName("debezium.sink.iceberg." + CatalogProperties.WAREHOUSE_LOCATION)
   String warehouseLocation();
 
+  @WithName("debezium.sink.iceberg.mapper")
+  @WithDefault("default-mapper")
+  String mapper();
+
   @WithName("debezium.sink.iceberg.destination-regexp")
 //    @WithDefault("")
   Optional<String> destinationRegexp();
